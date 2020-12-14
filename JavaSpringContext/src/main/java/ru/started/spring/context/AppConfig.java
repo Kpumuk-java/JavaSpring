c2fd4176a10c94959c5d05579f53db638e880d56
Slava <bag001@yandex.ru>
2020-12-14 14:23:01 +0300

    finished two lesson spring 2020.12.14

---
+++ JavaSpringContext/src/main/java/ru/started/spring/context/AppConfig.java
@@ -0,0 +1,11 @@
+package ru.started.spring.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.started.spring.context")
+public class AppConfig  {
+
+
+}
---
+++ JavaSpringContext/src/main/java/ru/started/spring/context/Product.java
@@ -0,0 +1,47 @@
+package ru.started.spring.context;
+
+public class Product {
+
+    private long ID;
+    private String name;
+    private double cost;
+    private static long count = 1;
+
+    public Product (String name, double cost) {
+        this.ID = count++;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public long getId() {
+        return ID;
+    }
+
+    public Product setId(long id) {
+        this.ID = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Product setCost(double cost) {
+        this.cost = cost;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%7d %20s %15.2f", ID, name, cost);
+    }
+}
