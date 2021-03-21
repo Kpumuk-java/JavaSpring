bc77eec1eb54c1bba91ca868dffd4ca25e665d47
Kpumuk <bag001@yandex.ru>
2021-03-21 12:28:15 +0300

    update lesson raddit 2021.03.21 12:28

--- console-rabbit-app/console-blog/src/main/java/ru/kpumuk/console/blog/producer/ProducerIT.java
+++ console-rabbit-app/console-blog/src/main/java/ru/kpumuk/console/blog/producer/ProducerIT.java
@@ -11,46 +11,45 @@ import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.LogManager;
 
 @Getter
 public class ProducerIT {
-    private List<String> EXCHANGE_LIST;
+    private Map<String, String> EXCHANGE_LIST;
     private ConnectionFactory factory;
-    private Logger log = org.slf4j.LoggerFactory.getLogger(ProducerIT.class);
-    private int count;
+    private static Logger log = org.slf4j.LoggerFactory.getLogger(ProducerIT.class);
+    private int COUNT;
 
-    public void addExchange (String exchangeName, String type) throws IOException, TimeoutException {
-         try (Connection connection = factory.newConnection();
-              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(exchangeName + type + count++, BuiltinExchangeType.DIRECT);
-
-            String routingKey = "com.gb.php.hello";
-            String message = "message123";
-
-            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+    public void publishMessage(String[] message) throws IOException, TimeoutException {
+        log.info("Start publish message: " + message[0] + " " + message[1]);
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            if (EXCHANGE_LIST != null && EXCHANGE_LIST.size() != 0) {
+                String exchangeName;
+                if (EXCHANGE_LIST.keySet().stream().equals(message[0])) {
+                    exchangeName = EXCHANGE_LIST.get(message[0]);
+                } else {
+                    exchangeName = message[1].substring(1,2) + COUNT++;
+                    EXCHANGE_LIST.put(message[0], exchangeName);
+                }
+                channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT);
+                channel.basicPublish(exchangeName, message[0], null, message[1].getBytes("UTF-8"));
+                log.info("Publish message " + message[1] + "in channel name " + exchangeName);
+            } else {
+                log.error("Can't publish message " + message[0] + " " + message[1]);
+            }
         }
     }
 
     public ProducerIT() {
         log.info("Init producer IT-blog rabbit");
-        EXCHANGE_LIST = new ArrayList<>();
+        EXCHANGE_LIST = new HashMap<>();
         factory = new ConnectionFactory();
         factory.setHost("localhost");
-        /*try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-
-            String routingKey = "com.gb.php.hello";
-            String message = "message123";
-
-            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
-        }*/
     }
-
 
 }
